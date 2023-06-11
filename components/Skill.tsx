fc91fbf21882f8bb1ec84da98898c55ae942a673
Luis Youn <hyoun1@kpmg.com.au>
2023-06-11 16:21:10 +1000

    add the open feature on skill sets

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,9 +1,8 @@
-import SkillItem from "./SkillItem";
 import Subtitle from "./Subtitle";
 
 import { FaTools } from 'react-icons/fa';
 import { TITLE_BACKEND, TITLE_FRONTEND, TITLE_LANGUAGE, TITLE_SKILL, TITLE_WEBDBSEERVER } from "@/repositories/Constants";
-import { slug } from "@/lib/slugify";
+import SkillSet from "./SkillSet";
 
 type Props = {
   frontends: SkillResultSet[],
@@ -13,31 +12,16 @@ type Props = {
 };
 
 export default function Skill({ languages, frontends, backends, webDbServers }: Props) {
-  const renderSkillset = (title: string, items: SkillResultSet[]) => {
-    return (
-      <div className="w-full sm:basis-1/2-gap-4 lg:basis-1/3-gap-4 xl:basis-1/4-gap-4">
-        <div className="p-4 cursor-pointer separator">
-          <h2 className={`text-md flex gap-2 transition`}>{title}</h2>
-        </div>
-        {items && items.length > 0 && items.map((item, index) => {
-          return (
-            <SkillItem key={index} item={item} id={slug(`${title} ${item.name}`)}/>
-          );
-        })}
-      </div>
-
-    );
-  }
   return (
     <>
       <Subtitle title={TITLE_SKILL}>
         <FaTools className="icon-primay" />
       </Subtitle>
       <div className="w-full flex flex-wrap gap-4 transition">
-        {renderSkillset(TITLE_LANGUAGE, languages)}
-        {renderSkillset(TITLE_FRONTEND, frontends)}
-        {renderSkillset(TITLE_BACKEND, backends)}
-        {renderSkillset(TITLE_WEBDBSEERVER, webDbServers)}
+        <SkillSet title={TITLE_LANGUAGE} items={languages} />
+        <SkillSet title={TITLE_FRONTEND} items={frontends} />
+        <SkillSet title={TITLE_BACKEND} items={backends} />
+        <SkillSet title={TITLE_WEBDBSEERVER} items={webDbServers} />
       </div>
     </>
   )
---
+++ components/SkillSet.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { slug } from "@/lib/slugify";
+import SkillItem from "./SkillItem";
+import Caret from "./Caret";
+import { useEffect, useRef, useState } from "react";
+import { openAccordion } from "@/lib/openAnimation";
+import clsx from "clsx";
+
+type Props = { title: string, items: SkillResultSet[] }
+
+export default function SkillSet({ title, items }: Props) {
+  const panelRef = useRef<HTMLDivElement>(null)
+  const [open, setOpen] = useState(true);
+
+  const handleOpen = () => open ? setOpen(false) : setOpen(true);
+
+  useEffect(() => {
+    openAccordion(panelRef, open);
+  }, [panelRef, open]);
+
+  return (
+    <div className="w-full relative sm:basis-1/2-gap-4 lg:basis-1/3-gap-4 xl:basis-1/4-gap-4">
+      <Caret open={open} onClick={handleOpen} />
+      <div className={clsx("p-4 cursor-pointer", open && 'separator')}>
+        <h2 className={`text-md flex gap-2 transition`}>{title}</h2>
+      </div>
+      <div ref={panelRef}
+        className={clsx('flex flex-wrap mt-2 transition-all duration-300')}>
+        {items && items.length > 0 && items.map((item, index) => {
+          return (
+            <SkillItem key={index} item={item} id={slug(`${title} ${item.name}`)} />
+          );
+        })}
+      </div>
+    </div>
+  )
+}
